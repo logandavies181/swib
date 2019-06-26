eaff2369d4af6d4f8ef04673dd7cee2dfd0225bb
logan vagrant <vagrant@localhost.localdomain>
2019-06-26 09:37:42 +0000

    back to where i was before

--- cmd/dump.go
+++ cmd/dump.go
@@ -4,11 +4,12 @@ import (
 	"fmt"
 	"net/http"
 	"io/ioutil"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
 
-var {
+var (
 	port string
 
 	method bool
@@ -22,14 +23,14 @@ var {
 	stdout bool
 	responseBody bool
 	bothOutputs bool
-}
+)
 
 // dumpCmd represents the dump command
 var dumpCmd = &cobra.Command{
 	Use:   "dump",
 	Short: "A simple http server for debugging purposes",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("Listening on port: %s", port)
+		fmt.Printf("Listening on port: %s\n", port)
 		
 		if all {
 			method = true
@@ -53,7 +54,7 @@ var dumpCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(dumpCmd)
-	dumpCmd.Flags().StringVarP(&port, "port", "p", 8080, "Port to listen on")
+	dumpCmd.Flags().StringVarP(&port, "port", "p", "8080", "Port to listen on")
 	
 	dumpCmd.Flags().BoolVarP(&method, "method", "m", false, "Print request method")
 	dumpCmd.Flags().BoolVarP(&path, "path", "P", false, "Print request path")
@@ -66,4 +67,42 @@ func init() {
 	dumpCmd.Flags().BoolVarP(&responseBody, "resp", "r", false, "Print to response body")
 	dumpCmd.Flags().BoolVarP(&bothOutputs, "", "A", false, "Shortcut to response to stdout and response body")
 
+}
+
+func dumpHandler (w http.ResponseWriter, r *http.Request) {
+		
+	var resp []string
+	var topLine []string
+
+	if method { topLine = append(topLine, r.Method) }
+	if path { topLine = append(topLine, r.URL.Path) }
+	if proto { topLine = append(topLine, r.Proto) }
+	
+	if len(topLine) > 0 {
+		resp = append(resp, strings.Join(resp," "))
+	}
+	
+	if headers {
+		var headerList []string
+		for k, v := range(r.Header) {
+			// TODO make sure this formats it like the actual request
+			headerList = append(headerList, fmt.Sprintf("%s: %s", k, strings.Join(v,", ")))
+		}
+		resp = append(resp, strings.Join(headerList, "\n"))
+	}
+	if body {
+		bodyData, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			fmt.Println(err)
+		}
+		resp = append(resp, string(bodyData))
+	}
+	
+	respString := strings.Join(resp, "\n")
+	if stdout {
+		fmt.Println(respString)
+	}
+	if responseBody {
+		fmt.Fprintln(w, respString)
+	}
 }
