1b0db9dd3d0b8fb78bcfe40d71e5510e2c05f1cd
logan vagrant <vagrant@localhost.localdomain>
2019-06-26 09:23:32 +0000

    implement variables

--- cmd/dump.go
+++ cmd/dump.go
@@ -14,6 +14,7 @@ var {
 	method bool
 	path bool
 	proto bool
+	host bool
 	headers bool
 	body bool
 	all bool
@@ -26,21 +27,43 @@ var {
 // dumpCmd represents the dump command
 var dumpCmd = &cobra.Command{
 	Use:   "dump",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "A simple http server for debugging purposes",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("dump called")
+		fmt.Printf("Listening on port: %s", port)
+		
+		if all {
+			method = true
+			path = true
+			proto = true
+			headers = true
+			host = true
+			body = true
+			
+		}
+		if bothOutputs {
+			stdout = true
+			responseBody = true
+		}
+		
+		// TODO handle errors here 
+		http.HandleFunc("/", dumpHandler)
+		http.ListenAndServe(":"+port, nil)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(dumpCmd)
+	dumpCmd.Flags().StringVarP(&port, "port", "p", 8080, "Port to listen on")
 	
-	dumpCmd.
+	dumpCmd.Flags().BoolVarP(&method, "method", "m", false, "Print request method")
+	dumpCmd.Flags().BoolVarP(&path, "path", "P", false, "Print request path")
+	dumpCmd.Flags().BoolVarP(&proto, "proto", "V", false, "Print request http version")
+	dumpCmd.Flags().BoolVarP(&headers, "headers", "H", false, "Print request headers")
+	dumpCmd.Flags().BoolVarP(&body, "body", "b", true, "Print request body")
+	dumpCmd.Flags().BoolVarP(&all, "all", "a", false, "Shortcut to print all")
+
+	dumpCmd.Flags().BoolVarP(&stdout, "stdout", "s", true, "Print to stdout")
+	dumpCmd.Flags().BoolVarP(&responseBody, "resp", "r", false, "Print to response body")
+	dumpCmd.Flags().BoolVarP(&bothOutputs, "", "A", false, "Shortcut to response to stdout and response body")
 
 }
